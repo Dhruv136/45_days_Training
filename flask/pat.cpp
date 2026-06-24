09de670e305025e2eadc43948a9e5fa664ec02b3
dhruv <kokchachinu136@gmail.com>
2026-06-24 09:16:07 +0530

    This is a commit regarding changes for matlplotlib and seaborn

---
+++ flask/pat.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i = 0;i<5;i++){
+        cout << " " <
+       for(int j=0;j<i;j++){
+
+        print("*");
+
+       } 
+    }
+    return 0;
+}
